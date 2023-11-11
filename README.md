4520e4b8bc84613e878cc507d19856446559ac74
Geraniol <1215918624@qq.com>
2023-11-12 00:08:53 +0800

    提交初步方案

--- README.md
+++ README.md
@@ -1,10 +1,50 @@
 # File Center <br /> 文件服务器  
+基于 Python 的轻量化、图形美化文件分享服务器  
 
-## **依赖**
-
-
-## **功能/使用说明**
-- 安装/运行
+## **功能/使用说明**  
+- 安装/运行  
+  - 进入待分享文件夹：`cd /path/to/dir`  
   - 克隆本存储库：`git clone https://github.com/Geraniol/FileServer.git`  
-  - 运行：`python3 mailcenter.py`  
+  - 运行：`python3 fileserver.py`  
+  - 从浏览器访问：`http://ip:port/`  
+- 快速分享  
+  - 在 `~/.bashrc` 或 `~/.zshrc` 中添加别名：`alias fs='python3 /path/to/fileserver.py'`  
+- 默认挂载至 `0.0.0.0:80`  
+  - 可在 `fileserver.py` 中修改  
+  - 修改为 `127.0.0.1`：仅本机可访问  
 
+## **参数**  
+- 程序 `fileserver.py` 中可自定义参数  
+- 内部参数  
+  - `SERVER_NAME` = `Shiro-chan` 服务器名称  
+  - `FILTER` = `["MP4", "MOV", "MKV"]` 文件过滤器  
+- 外部参数  
+  - `PATH_INDEX` = `./lib/index.html` 网页框架模版之文件路径  
+  - `PATH_SAMPLE` = `./lib/sample.html` 网页模块模版之文件路径  
+  - `PATH_ROOT` = `..` 服务器主目录路径  
+
+## **改进方向**  
+- 支持文件夹树状图搜索  
+- 支持修改模版（`HTML`、`CSS`）  
+- 支持自定义输出、端口等参数  
+- 代码待优化  
+- 优化项目目录结构  
+- 支持添加为网页应用  
+- 提供更多文件过滤选项  
+- 解决 `gethostbyname_ex` 在部分环境中的异常  
+- 支持 `HTTPS` 协议  
+- 支持密码访问（实验性功能）  
+
+## **区别**  
+- 与 `python3 -m http.server`  
+  - 美化的的网页界面  
+- 与 `beautify-http-server`  
+  - 提供安全权限控制  
+  - 尚不支持上传文件  
+- 与 `Apache`、`Nginx` 或 `Flask` 等  
+  - 无需安装依赖、框架、配置服务器  
+
+## **问题**  
+- 提示权限不足  
+  - 更改所用端口（1024+）  
+  - 使用管理员权限运行：`sudo python3 fileserver.py`  
---
+++ fileserver.py
@@ -0,0 +1,145 @@
+def run(port=80, pwd=""):
+    from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+    from socket import gethostbyname_ex, gethostname
+    from urllib.parse import unquote
+    import json
+    import os
+    import time
+
+    SERVER_NAME = "Shiro-chan"
+
+    PATH_INDEX = "./lib/index.html"
+    PATH_SAMPLE = "./lib/sample.html"
+    PATH_ROOT = ".."
+
+    FILTER = ["MP4", "MOV", "MKV"]
+
+    class SuperHTTPServer(BaseHTTPRequestHandler):
+
+        def send_server_info(self):
+            self.send_header("Connection", "close")
+            self.send_header("Date", time.strftime("%Y.%m.%d %H:%M:%S %z"))
+            self.send_header("Server", SERVER_NAME)
+
+
+        def response403(self):
+            self.send_response_only(403, "Forbidden")
+            self.send_server_info()
+            self.send_header("Content-Type", "application/json; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(json.dumps({
+                "ok": False,
+                "status_code": 403,
+                "message": "未授权访问"
+            }, ensure_ascii=False).encode("utf-8"))
+
+        def response500(self):
+            self.send_response_only(500, "Internal Server Error")
+            self.send_server_info()
+            self.send_header("Content-Type", "application/json; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(json.dumps({
+                "ok": False,
+                "status_code": 500,
+                "message": "服务器错误"
+            }, ensure_ascii=False).encode("utf-8"))
+
+        def response200(self):
+            self.send_response_only(200, "OK")
+            self.send_server_info()
+            self.send_header("Server", SERVER_NAME)
+
+        def IsAccessible(self, path):
+            accessible = [f"/{x}" for x in os.listdir(f"{PATH_ROOT}") if os.path.isfile(f"{PATH_ROOT}/{x}") and not x.startswith(".") and x.split(".")[-1].upper() in FILTER] + ["./lib/icon.png"]
+            if path in accessible:
+                return True
+            else:
+                return False
+
+        def sendfile(self, path):
+            try:
+                if path == "/lib/icon.png":
+                    with open(f".{path}", "rb") as f:
+                        c = f.read()
+                else:
+                    with open(f"{PATH_ROOT}{path}", "rb") as f:
+                        c = f.read()
+
+                self.response200()
+                self.send_header("Content-Type", "application/file; charset=utf-8")
+                self.send_header("Content-Length", str(len(c)))
+                self.end_headers()
+                self.wfile.write(c)
+            except:
+                self.response500()
+
+        def sendpage(self):
+            try:
+                with open(PATH_INDEX, "rt") as f:
+                    index = f.read()
+                with open(PATH_SAMPLE, "rt") as f:
+                    sample = f.read()
+
+                page = index.split("{{BREAKPOINT}}")[0]
+
+                for filename in sorted([x for x in os.listdir(f"{PATH_ROOT}") if os.path.isfile(f"{PATH_ROOT}/{x}") and not x.startswith(".") and x.split(".")[-1].upper() in FILTER]):
+                    filetime = time.strftime("%m/%d %H:%M", time.localtime(os.path.getctime(f"{PATH_ROOT}/{filename}")))
+                    filesize = f"{os.path.getsize(f'{PATH_ROOT}/{filename}')/10**6:.1f} MB"
+                    filetype = filename.split(".")[-1].upper()
+
+                    label = sample.replace("{{FILENAME}}", filename).replace("{{FILETIME}}", filetime).replace("{{FILESIZE}}", filesize).replace("{{FILETYPE}}", filetype).replace("{{SERVERHOST}}", str(host)).replace("{{SERVERPOST}}", str(port))
+
+                    page += label
+
+                page += index.split("{{BREAKPOINT}}")[1]
+                page = page.encode("utf-8")
+
+                self.response200()
+                self.send_header("Content-Type", "text/html; charset=utf-8")
+                self.send_header("Content-Length", len(page))
+                self.end_headers()
+                self.wfile.write(page)
+            except:
+                self.response500()
+
+        def do_GET(self):
+
+            path = unquote(self.path)
+
+            print(f"{self.address_string()} GET '{path}'")
+
+            if pwd:
+                try:
+                    usrpwd = self.headers["pwd"]
+                except:
+                    usrpwd = ""
+                if pwd != usrpwd:
+                    self.response403()
+                    return
+
+            if path == "/":
+                self.sendpage()
+                return
+
+            if self.IsAccessible(path):
+                self.sendfile(path)
+            else:
+                self.response403()
+                return
+
+        def do_POST(self):
+
+            path = unquote(self.path)
+
+            print(f"{self.address_string()} POST '{path}'")
+
+            self.response403()
+
+    host = gethostbyname_ex(gethostname())[2][-1]
+    print(f"Host: {host}, Port: {port}.")
+    httpd = ThreadingHTTPServer(("", port), SuperHTTPServer)
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    run(port=80, pwd="")
